0f53cc70ce987d1f2b79e298a3f949af094863bf
iamagf <agf0710@gmail.com>
2024-04-29 02:06:05 -0600

    Removed bootstrap and added basic functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { AdenaGetAccount } from './AdenaInterfaces';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-function App() {
+type Props = {
+  adena?: any;
+};
+
+
+function App({ adena }: Props) {
+  const [data, setData] = useState<AdenaGetAccount | null>(null)
+  const [CallExamsAvailable, setCallExamsAvailable] = useState<any | null>(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Getting information of the user and the address involved
+        const valueGetAccount = await adena.GetAccount();
+        setData(valueGetAccount);
+        console.log(valueGetAccount)
+        const pendingExams = `http://127.0.0.1:8888/r/dev/shikenrepository:NumberPendingExams/${valueGetAccount.data.address}`
+
+        axios.get(pendingExams).then(function (response: any) {
+          const tempElement = document.createElement('div');
+          tempElement.innerHTML = response.data;
+          const realmRender = tempElement.querySelector('#realm_render');
+          const availableExams = realmRender ? realmRender.textContent : "";
+          console.log(availableExams);
+          setCallExamsAvailable(availableExams);
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>The address registered is: <code>{data ? data.data.address : "Fetching information..."} ({data ? data.data.coins : ""})</code></h2>
+        <hr />
+        <h2>Number of Pending Exams: {CallExamsAvailable ? CallExamsAvailable : ""}</h2>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.onload = async () => {
+  await new Promise(resolve => setTimeout(resolve, 100));
+  //look for the adena object
+  if (!(window as any).adena) {
+    //open adena.app in a new tab if the adena object is not found
+    window.open("https://adena.app/", "_blank");
+  } else {
+    const root = ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    );
+    root.render(<App adena={(window as any).adena} />);
+  }
+};
