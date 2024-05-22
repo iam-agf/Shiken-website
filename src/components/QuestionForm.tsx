7197ba4071a79841811790c8f9884fbcf644f7ed
iam-agf <agf0710@gmail.com>
2024-05-21 23:46:43 -0600

    Question form added with AES encrypt

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -1,50 +1,38 @@
 import { useContext, useEffect, useState } from "react";
-import { Question } from "../pieces/Realm.types";
+import { Question, Crypto } from "../pieces/Realm.types";
 import { AdenaService } from "../services/adena/adena";
 import { EMessageType } from "../services/adena/adena.types";
 import AccountContext from "../context/AccountContext";
 import config from "../config";
 import forge from 'node-forge';
 import "../style.css"
-
-type KindValue = "0" | "1" | "2";
-const openQ = "2" as KindValue
-const TrueFalseQ = "1" as KindValue
+import { encryptMessage } from "../pieces/supportFuns";
 
 const QuestionForm = () => {
     const { address } = useContext(AccountContext)
+    const [cryptoData, setCryptoData] = useState<Crypto>({ salt: "", randomAES: "" })
+    const [question, setQuestion] = useState<Question>({
+        topic: "",
+        statement: "",
+        kind: "1",
+        options: "",
+        answer: "",
+        hashAES: "",
+    })
+    const [encryptedQuestion, setEncryptedQuestion] = useState<Question>({
+        topic: "",
+        statement: "",
+        kind: "",
+        options: "",
+        answer: "",
+        hashAES: "",
+    })
 
-    // Question data
-    const [statement, setStatement] = useState<string>("")
-    const [kind, setKind] = useState<KindValue>("1")
     // Options. Will join them in Options
     const [Option1, setOption1] = useState("1")
     const [Option2, setOption2] = useState("2")
     const [Option3, setOption3] = useState("3")
     const [Option4, setOption4] = useState("4")
-    const [Options, setOptions] = useState("1,2,3,4")
-    const [answer, setAnswer] = useState("2")
-
-    // Encryption values
-    const [AES, setAES] = useState("")
-    const [AESHex, setAESHex] = useState("")
-    const [pub, setPub] = useState<string>()
-    const [priv, setPriv] = useState<string>()
-    const [pubKey, setPubKey] = useState<forge.pki.rsa.PublicKey>()
-
-    // Encrypted data
-    const [eStatement, setEStatement] = useState(statement)
-    const [eAnswer, setEAnswer] = useState(answer)
-    const [eOptions, setEOptions] = useState(Options)
-
-    // Generates the RSA keys for the encryption process
-    useEffect(() => {
-        // RSA keys
-        const { publicKey, privateKey }: forge.pki.rsa.KeyPair = forge.pki.rsa.generateKeyPair(2048, 0x10001);
-        setPub(forge.pki.publicKeyToPem(publicKey))
-        setPubKey(publicKey)
-        setPriv(forge.pki.privateKeyToPem(privateKey))
-    }, [])
 
     // Updates information related to options
     useEffect(() => {
@@ -52,85 +40,103 @@ const QuestionForm = () => {
         if (Option1 !== "") {
             options = `${Option1},${Option2},${Option3},${Option4}`
         }
-        setOptions(options)
+        setQuestion(prev => ({ ...prev, options: options }))
     },
         [Option1, Option2, Option3, Option4])
 
     // Updates the options and answer based on kind
     useEffect(() => {
-        if (kind === "0") {
+        if (question.kind === "0") {
             setOption1("1")
             setOption2("2")
             setOption3("3")
             setOption4("4")
         }
-        if (kind === "1") {
+        if (question.kind === "1") {
             setOption1("True")
             setOption2("False")
             setOption3("Can't be determined")
             setOption4("No answer")
         }
-        if (kind === "2") {
+        if (question.kind === "2") {
             setOption1("")
             setOption2("")
             setOption3("")
             setOption4("")
-            setOptions("")
-            setAnswer("")
+            setQuestion(prev => ({ ...prev, options: "", answer: "" }))
         }
-    }, [kind])
+    }, [question.kind])
 
     // AES key for user
     useEffect(() => {
-
-     }, [AES])
-
-    // encrypts all
-    useEffect(() => {
-        if (pubKey) {
-            let eS: string = forge.util.encode64(pubKey!.encrypt(statement))
-            let eO: string = forge.util.encode64(pubKey!.encrypt(Options))
-            let eA: string = forge.util.encode64(pubKey!.encrypt(answer))
-            setEAnswer(eA)
-            setEStatement(eS)
-            setEOptions(eO)
+        generateKeys()
+        console.log({ cryptoData })
+        if (cryptoData.randomAES !== "" && cryptoData.salt !== "") {
+            encryptRandomAES();
+            const parts = cryptoData.randomAES.split("@");
+            const bytesKey = forge.util.hexToBytes(parts[0]);
+            const randomBytesIV = forge.util.hexToBytes(parts[1]);
+            encryptQuestion(bytesKey, randomBytesIV);
         }
-    }, [answer, Options, statement])
+    }, [cryptoData.salt, question])
+
+    function generateKeys() {
+        const randomBytesKey = forge.random.getBytesSync(32);
+        const randomBytesIV = forge.random.getBytesSync(32);
+
+        // For storing purposes
+        const stringKey = forge.util.bytesToHex(randomBytesKey);
+        const stringIV = forge.util.bytesToHex(randomBytesIV);
+        const storingAESCombiation = `${stringKey}@${stringIV}`;
+        setCryptoData(prevState => ({ ...prevState, randomAES: storingAESCombiation }));
+        return
+    }
+
+    function encryptRandomAES() {
+        // salt is SHA256-ed and used both parts to encrypt the randomAES
+        const md = forge.md.sha256.create();
+        md.update(cryptoData.salt);
+        const shaString = md.digest().toHex();
+        const shaKey = shaString.substring(0, 32);
+        const shaIV = shaString.substring(32, 64);
+        const encriptedRandomAES = encryptMessage(
+            cryptoData.randomAES,
+            shaKey,
+            shaIV
+        )
+        // Then the encrypted content is set
+        setEncryptedQuestion(prev => ({ ...prev, hashAES: encriptedRandomAES }));
+    }
+
+    function encryptQuestion(Key: string, randomBytesIV: string) {
+        setEncryptedQuestion(prev => ({
+            ...prev,
+            topic: encryptMessage(question.topic, Key, randomBytesIV),
+            statement: encryptMessage(question.statement, Key, randomBytesIV),
+            options: encryptMessage(question.options, Key, randomBytesIV),
+            kind: encryptMessage(question.kind, Key, randomBytesIV),
+            answer: encryptMessage(question.answer, Key, randomBytesIV),
+        }));
+    }
 
     const sendQuestion = async () => {
-        let q: Question = {
-            statement: "",
-            kind: "2",
-            options: "",
-            answer: "",
-        }
-        if (statement.length < 5) {
+        if (question.statement.length < 5) {
             console.error("Size of statement is very short")
-        } else {
-            q.statement = statement
         }
-        let kindString: string = kind as string
-        if (kindString !== "1" && kindString !== "2" && kindString !== "0") {
+        if (question.kind !== "1" && question.kind !== "2" && question.kind !== "0") {
             console.error("Kind isn't correct")
-        } else {
-            q.kind = kind as string
         }
-        if (Option1 === "" || Option2 === "" || Option3 === "" || Option4 === "") {
+        if (question.kind !== "2" && (Option1 === "" || Option2 === "" || Option3 === "" || Option4 === "")) {
             console.error("An option is empty")
-        } else {
-            // This Options variable is already built
-            q.options = Options
         }
-        if (kindString !== "2" && (answer !== Option1 && answer !== Option2 && answer !== Option3 && answer !== Option4)) {
+        const options = [Option1, Option2, Option3, Option4];
+        if (question.kind !== "2" && !options.includes(question.answer)) {
             console.error("Answer not in options")
-        } else {
-            q.answer = answer
         }
-        if (address) {
-            if (q.kind === "1") {
-                q.options = `"True","False","Can't be determined","No answer"`
+        if (address !== null) {
+            if (question.kind === "1") {
+                question.options = `"True","False","Can't be determined","No answer"`
             }
-            console.log(q)
             let response = await AdenaService.sendTransaction(
                 [
                     {
@@ -141,10 +147,12 @@ const QuestionForm = () => {
                             pkg_path: config.REALM_PATH,
                             func: 'AddQuestion',
                             args: [
-                                q.statement,
-                                q.kind,
-                                q.options,
-                                q.answer
+                                encryptedQuestion.topic,
+                                encryptedQuestion.statement,
+                                encryptedQuestion.kind,
+                                encryptedQuestion.options,
+                                encryptedQuestion.answer,
+                                encryptedQuestion.hashAES,
                             ]
                         }
                     }
@@ -165,7 +173,7 @@ const QuestionForm = () => {
                 <label>Encryption password (This password is only responsibility of the creator, and shouldn't be shared):
                     <input
                         type="text"
-                        onChange={(e) => setAES(e.target.value)}
+                        onChange={(e) => setCryptoData(prev => ({ ...prev, salt: e.target.value }))}
                     />
                 </label>
                 <br />
@@ -173,21 +181,28 @@ const QuestionForm = () => {
                 <label>Enter your Question:
                     <input
                         type="text"
-                        value={statement}
-                        onChange={(e) => setStatement(e.target.value)}
-                    />
+                        value={question.statement}
+                        onChange={(e) => setQuestion(prev => ({ ...prev, statement: e.target.value }))} />
+                </label>
+                <br />
+                <hr />
+                <label>Enter your Topic:
+                    <input
+                        type="text"
+                        value={question.topic}
+                        onChange={(e) => setQuestion(prev => ({ ...prev, topic: e.target.value }))} />
                 </label>
                 <br />
                 <hr />
                 <label>Enter the kind of question:
-                    <input type='radio' name='questionKind' id='0' value='0' onChange={e => setKind(e.target.value as KindValue)} />
-                    <input type='radio' name='questionKind' id='1' value='1' onChange={e => setKind(e.target.value as KindValue)} />
-                    <input type='radio' name='questionKind' id='2' value='2' onChange={e => setKind(e.target.value as KindValue)} />
+                    <input type='radio' name='questionKind' id='0' value='0' onChange={e => setQuestion(prev => ({ ...prev, kind: e.target.value }))} />
+                    <input type='radio' name='questionKind' id='1' value='1' onChange={e => setQuestion(prev => ({ ...prev, kind: e.target.value }))} />
+                    <input type='radio' name='questionKind' id='2' value='2' onChange={e => setQuestion(prev => ({ ...prev, kind: e.target.value }))} />
                 </label>
                 <br />
-                {kind !== openQ ?
+                {question.kind !== "2" ?
                     <>
-                        {kind !== TrueFalseQ ?
+                        {question.kind !== "1" ?
                             <>
                                 <hr />
                                 <label>Enter Options:
@@ -199,20 +214,20 @@ const QuestionForm = () => {
                                 <br />
                                 <hr />
                                 <label>Choose answer
-                                    <input type='radio' name='answer' id={`${Option1}`} value={Option1} onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id={`${Option2}`} value={Option2} onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id={`${Option3}`} value={Option3} onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id={`${Option4}`} value={Option4} onChange={e => setAnswer(e.target.value)} />
+                                    <input type='radio' name='answer' id={`${Option1}`} value={Option1} onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id={`${Option2}`} value={Option2} onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id={`${Option3}`} value={Option3} onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id={`${Option4}`} value={Option4} onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
                                 </label>
                             </>
                             :
                             <>
                                 <hr />
-                                <label>Choose answer (TF)
-                                    <input type='radio' name='answer' id="True" value="True" onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id="False" value="False" onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id="Can't be determined" value="Can't be determined" onChange={e => setAnswer(e.target.value)} />
-                                    <input type='radio' name='answer' id="No answer" value="No answer" onChange={e => setAnswer(e.target.value)} />
+                                <label>Choose answer (True-False)
+                                    <input type='radio' name='answer' id="True" value="True" onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id="False" value="False" onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id="Can't be determined" value="Can't be determined" onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
+                                    <input type='radio' name='answer' id="No answer" value="No answer" onChange={e => setQuestion(prev => ({ ...prev, answer: e.target.value }))} />
                                 </label>
                                 <br />
                             </>
@@ -229,19 +244,19 @@ const QuestionForm = () => {
                 <br />
                 <hr />
                 <h4>Statement: </h4>
-                <p>{eStatement}</p>
+                <p>{encryptedQuestion.statement}</p>
                 <br />
                 <hr />
                 <h4>Kind:</h4>
-                <p>{kind}</p>
+                <p>{encryptedQuestion.kind}</p>
                 <br />
                 <hr />
                 <h4>Options:</h4>
-                <p>{eOptions}</p>
+                <p>{encryptedQuestion.options}</p>
                 <br />
                 <hr />
                 <h4>Answer:</h4>
-                <p>{eAnswer}</p>
+                <p>{encryptedQuestion.answer}</p>
                 <br />
                 <hr />
             </div>
